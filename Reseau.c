330476ff9f56558ea9424260de3027c2d93c847a
BshKatrin <dyanko.ekaterina2015@gmail.com>
2024-03-07 15:04:04 +0100

    exo2

--- Reseau.c
+++ Reseau.c
@@ -1,9 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
 
 #include "SVGLib/SVGwriter.h"
-
+#include "Chaine.h"
 #include "Reseau.h"
+
+#define EPS 1e-3 // precision pour comparaison de l'egalite entre double
 
 int nbCommodites(Reseau *R)
 {
@@ -12,7 +16,11 @@ int nbCommodites(Reseau *R)
     int cmt = 0;
     CellCommodite *noeud_cour = R->commodites;
     while (noeud_cour)
+    {
         cmt++;
+        noeud_cour = noeud_cour->suiv;
+    }
+
     return cmt;
 }
 
@@ -53,7 +61,6 @@ void ecrireReseau(Reseau *R, FILE *f)
 
     // Ajouter newline
     fputc(10, f);
-
     CellNoeud *noeud_cour = R->noeuds;
     Noeud *noeud = NULL;
     // Ecrire les points
@@ -64,7 +71,6 @@ void ecrireReseau(Reseau *R, FILE *f)
         noeud_cour = noeud_cour->suiv;
     }
     fputc(10, f);
-
     // Ecrire des liaisons
     noeud_cour = R->noeuds;
     CellNoeud *voisin = NULL;
@@ -81,7 +87,6 @@ void ecrireReseau(Reseau *R, FILE *f)
         noeud_cour = noeud_cour->suiv;
     }
     fputc(10, f);
-
     // Ecrire des commodites
     CellCommodite *commodite = R->commodites;
     while (commodite)
@@ -91,7 +96,7 @@ void ecrireReseau(Reseau *R, FILE *f)
     }
 }
 
-void afficheReseauSVG(Reseau *R, char *nomInstance)
+void affichenoeud_nouveauSVG(Reseau *R, char *nomInstance)
 {
     CellNoeud *noeud_cour, *voisin;
     SVGwriter svg;
@@ -125,4 +130,131 @@ void afficheReseauSVG(Reseau *R, char *nomInstance)
         noeud_cour = noeud_cour->suiv;
     }
     SVGfinalize(&svg);
+}
+
+// Retourne true ssi les noeuds sont egaux
+bool noeuds_egaux(Noeud *n1, Noeud *n2)
+{
+    return (fabs(n1->x - n2->x) < EPS) && (fabs(n1->y - n2->y) < EPS);
+}
+
+Noeud *rechercheCreeNoeudListe(Reseau *R, double x, double y)
+{
+    if (!R)
+        return NULL;
+    // Vérification de la présence du noeud
+    CellNoeud *cell_cour = R->noeuds;
+    while (cell_cour)
+    {
+        if ((fabs(cell_cour->nd->x - x) < EPS) && (fabs(cell_cour->nd->y - y) < EPS))
+            return cell_cour->nd;
+        cell_cour = cell_cour->suiv;
+    }
+
+    //   Ajout en cas d'absence, je crée le noeud
+    Noeud *noeud_nouv = malloc(sizeof(Noeud));
+    noeud_nouv->x = x;
+    noeud_nouv->y = y;
+    noeud_nouv->num = R->nbNoeuds + 1;
+    noeud_nouv->voisins = NULL;
+    (R->nbNoeuds)++;
+
+    // et le rajoute à la liste des noeuds du réseau
+    CellNoeud *cell_noeud_nouv = malloc(sizeof(CellNoeud));
+    cell_noeud_nouv->nd = noeud_nouv;
+    // Ajout en tete
+    cell_noeud_nouv->suiv = R->noeuds;
+    R->noeuds = cell_noeud_nouv;
+    return noeud_nouv;
+}
+
+// Creer commodite, l'ajouter en tete des commodites du reseau
+void ajouter_commodite(Reseau *R, Noeud *extrA, Noeud *extrB)
+{
+    CellCommodite *commodite = malloc(sizeof(CellCommodite));
+    commodite->extrA = extrA;
+    commodite->extrB = extrB;
+    commodite->suiv = R->commodites;
+    R->commodites = commodite;
+}
+
+// Creer cellnoeud qui point vers noeud donne
+CellNoeud *creer_cell_noeud(Reseau *R, Noeud *nd)
+{
+    CellNoeud *cell_nouv = malloc(sizeof(CellNoeud));
+    cell_nouv->nd = nd;
+    cell_nouv->suiv = NULL;
+    return cell_nouv;
+}
+
+// Verifier si voisins contient deja le noeud. Retourne pointeur vers le cellnoeud trouve s'il existe, sinon NULL
+CellNoeud *recherche_voisin(CellNoeud *liste_voisins, Noeud *n)
+{
+    if (!liste_voisins)
+        return NULL;
+
+    CellNoeud *cour = liste_voisins;
+    while (cour && (!noeuds_egaux(cour->nd, n)))
+        cour = cour->suiv;
+    return cour;
+}
+
+Reseau *reconstitueReseauListe(Chaines *C)
+{
+    // Création du réseau
+    Reseau *res = malloc(sizeof(Reseau));
+    res->nbNoeuds = 0;
+    res->gamma = C->gamma;
+    res->noeuds = NULL;
+    res->commodites = NULL;
+
+    // Parcours des chaines
+    CellChaine *chaine_cour = C->chaines;
+    CellPoint *point_cour = NULL;
+
+    // Pour voisins
+    Noeud *noeud_prec = NULL;
+    Noeud *noeud_trouve = NULL;
+    CellNoeud *nouv = NULL;
+
+    // Pour commodites
+    Noeud *extrA = NULL;
+    Noeud *extrB = NULL;
+
+    while (chaine_cour)
+    {
+        point_cour = chaine_cour->points;
+        while (point_cour)
+        {
+            noeud_trouve = rechercheCreeNoeudListe(res, point_cour->x, point_cour->y);
+
+            // MAJ des voisins
+            if (noeud_prec)
+            {
+                // if noeud_cour_voisins contient deja noeud_prec, faire rien
+                // if noeud_prec_voisins contient deja noeud_cour, faire rien
+                // Sinon, faire ajout de noeud_prec dans le noeud_cour_voisins
+                if ((!recherche_voisin(noeud_trouve->voisins, noeud_prec)) && (!recherche_voisin(noeud_prec->voisins, noeud_trouve)))
+                {
+                    nouv = creer_cell_noeud(res, noeud_prec);
+                    nouv->suiv = noeud_trouve->voisins;
+                    noeud_trouve->voisins = nouv;
+                }
+            }
+            // Definir le premier point comme extrA
+            if (extrA == NULL)
+                extrA = noeud_trouve;
+
+            noeud_prec = noeud_trouve;
+            point_cour = point_cour->suiv;
+        }
+        //  Le noeud correpond au dernier point
+        extrB = noeud_trouve;
+        ajouter_commodite(res, extrA, extrB);
+
+        chaine_cour = chaine_cour->suiv;
+        extrA = NULL;
+        noeud_prec = NULL;
+    }
+    return res;
 }
